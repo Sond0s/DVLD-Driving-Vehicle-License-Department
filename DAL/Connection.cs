1e331d3804e0695a291c3e08953b1d75a3b0e32d
Sond0s <sondoskhalid79@gmail.com>
2026-07-27 20:26:18 +0300

    Initial Commit

---
+++ DAL/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class Connection
+    {
+        public static string connectionString = "Server=.;Database=DVLD;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
